938fb572f6511414966a5125e8dc21881ce67238
franklsf95 <franklsf95@gmail.com>
2013-04-24 23:52:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+offer
+=====
+
+An online offer-tracking system for RDFZ 2013
+
+PHP version: http://franklsf.org/offer
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfferRails::Application.config.secret_token = 'a2232136b953ab61661358f7de0266ff4bac202417781bebafb54dee683f216e9f77ba99a903c50f6dd078ac61915966941c5a9334f5d55e4abb426daf81c2a4'
